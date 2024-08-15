6968791c3bb153558242e7d49ed8aad932a60dd4
Alexander Petkov <iskander9207@icloud.com>
2024-08-15 11:07:46 +0300

    Styles reseting, adding google font and creating the folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1 className="text-3xl font-bold">Hello :)</h1>
-        </main>
-    );
+    return <h1 className="text-3xl font-bold">NextJS Store 🛒 tutorial :)</h1>;
 }
