0a3445f77b359a427e08118bad1af8b75d2e516e
Alexander Petkov <iskander9207@icloud.com>
2024-08-15 14:51:08 +0300

    Update Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,15 +5,10 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export function Nav({ children }: { children: React.ReactNode }) {
-    return <nav className="p-2 text-[18px] text-white bg-slate-800 flex flex-row justify-center gap-10">{children}</nav>;
+    return <nav className="text-[18px] text-white bg-slate-800 flex flex-row justify-center">{children}</nav>;
 }
 
 export function NavLink(props: Omit<ComponentProps<typeof Link>, "className">) {
     const pathname = usePathname();
-    return (
-        <Link
-            {...props}
-            className={`text-white hover:text-slate-800 hover:bg-white rounded px-2 py-1 ${pathname === props.href && "bg-white text-slate-800"}`}
-        />
-    );
+    return <Link {...props} className={`text-white hover:text-slate-800 hover:bg-white px-4 py-4 ${pathname === props.href && "bg-white text-slate-800"}`} />;
 }
