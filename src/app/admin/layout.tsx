ce2b0f56f173ef99a06a30b9f1e36cb17f73f28a
Alexander Petkov <iskander9207@icloud.com>
2024-08-15 14:44:32 +0300

    finished navbar

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,7 +1,14 @@
+import { Nav, NavLink } from "@/components/Nav";
+
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
-            <Nav></Nav>
+            <Nav>
+                <NavLink href="/admin">Dashboard</NavLink>
+                <NavLink href="/products">Products</NavLink>
+                <NavLink href="/users">Customers</NavLink>
+                <NavLink href="/sales">Sales</NavLink>
+            </Nav>
             <div>{children}</div>
         </>
     );
---
+++ src/components/Nav.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { ComponentProps } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export function Nav({ children }: { children: React.ReactNode }) {
+    return <nav className="p-2 text-[18px] text-white bg-slate-800 flex flex-row justify-center gap-10">{children}</nav>;
+}
+
+export function NavLink(props: Omit<ComponentProps<typeof Link>, "className">) {
+    const pathname = usePathname();
+    return (
+        <Link
+            {...props}
+            className={`text-white hover:text-slate-800 hover:bg-white rounded px-2 py-1 ${pathname === props.href && "bg-white text-slate-800"}`}
+        />
+    );
+}
