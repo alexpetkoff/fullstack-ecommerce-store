c8b0b774cc766dfa5933f467466423285ebf6b71
Alexander Petkov <iskander9207@icloud.com>
2024-08-20 16:52:00 +0300

    created products page

---
+++ src/app/admin/products/page.tsx
@@ -0,0 +1,9 @@
+import { PageHeading } from "@/components/PageHeading";
+
+export default function ProductsPage() {
+    return (
+        <div className="mt-10">
+            <PageHeading>Products</PageHeading>
+        </div>
+    );
+}
