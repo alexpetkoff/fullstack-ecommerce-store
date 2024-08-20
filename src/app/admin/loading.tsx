0395c251a349a5d57a83580e96d46fd70e9984ab
Alexander Petkov <iskander9207@icloud.com>
2024-08-20 13:23:08 +0300

    finished admin dashboard page + added loading.tsx

---
+++ src/app/admin/loading.tsx
@@ -0,0 +1,3 @@
+export default function AdminLoading() {
+    return <div className="mt-10 text-center text-2xl text-black animate-bounce">Loading...</div>;
+}
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,11 +2,17 @@ import prisma from "@/db/db";
 import Card from "@/components/Card";
 import { formatCurrency, formatNumber } from "@/lib/formatters";
 
+function wait(time: number) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
 async function getSalesData() {
     const data = await prisma?.order.aggregate({
         _sum: { pricePaidInCents: true },
         _count: true,
     });
+
+    await wait(2000);
 
     return {
         amount: (data?._sum.pricePaidInCents || 0) / 100,
@@ -14,18 +20,46 @@ async function getSalesData() {
     };
 }
 
-async function getUserData() {}
+async function getUserData() {
+    const [userCount, orderData] = await Promise.all([
+        prisma?.user.count(),
+        prisma?.order.aggregate({
+            _sum: { pricePaidInCents: true },
+        }),
+    ]);
+
+    return {
+        userCount,
+        averageValuePerUser: userCount === 0 ? 0 : (orderData?._sum.pricePaidInCents || 0) / userCount / 100,
+    };
+}
+
+async function getProductsData() {
+    const [activeCount, inactiveCount] = await Promise.all([
+        prisma?.product.count({
+            where: { isAvailableForPurchase: true },
+        }),
+        prisma?.product.count({
+            where: { isAvailableForPurchase: false },
+        }),
+    ]);
+
+    return {
+        activeCount,
+        inactiveCount,
+    };
+}
 
 export default async function AdminDashboard() {
-    const salesData = await getSalesData();
+    const [salesData, usersData, productsData] = await Promise.all([getSalesData(), getUserData(), getProductsData()]);
 
     return (
         <div className="m-10">
             <h1 className="text-3xl font-bold text-center">Admin Dashboard</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-6 px-10 gap-4">
                 <Card title="Sales" subtitle={formatNumber(salesData?.numberOfSales) + " Orders"} body={formatCurrency(salesData?.amount)} />
-                <Card title="Customers" subtitle="100$ Average value" body="1" />
-                <Card title="Active products" subtitle="1 inactive" body="7" />
+                <Card title="Customers" subtitle={`${formatCurrency(usersData.averageValuePerUser)} Average value`} body={formatNumber(usersData.userCount)} />
+                <Card title="Active products" subtitle={`${formatNumber(productsData.inactiveCount)} Inactive`} body={formatNumber(productsData.activeCount)} />
             </div>
         </div>
     );
